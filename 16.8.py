f3fccdc03baf0a923a080b00e83053635d756cf8
Becky Rieger <brieger@ivytech.edu>
2024-11-13 20:34:01 -0600

    fxer

--- 16.8.py
+++ 16.8.py
@@ -5,10 +5,6 @@ import sqlite3
 conn = sqlite3.connect ('books.db')
 cursor = conn.cursor()
 
-cursor.execute('''
-CREATE TABLE books (title TEXT, author TEXT, year INTEGER)
-''')
-
 conn.commit()
 conn.close()
 
