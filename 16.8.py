dcf639eca0705ea4a127808b31a669bd16adf549
Becky Rieger <brieger@ivytech.edu>
2024-11-13 20:23:31 -0600

    fix again

--- 16.8.py
+++ 16.8.py
@@ -1,5 +1,5 @@
-#16.8 assignment
-#Use the sqlalchemy module to connect to the sqlite3 database books.db that you just made in exercise 16.4. As in 16.6, select and print the title column from the book table in alphabetical order.
+# 16.8 assignment
+# Use the sqlalchemy module to connect to the sqlite3 database books.db that you just made in exercise 16.4. As in 16.6, select and print the title column from the book table in alphabetical order.
 import sqlite3
 cursor.execute('''
 CREATE TABLE books (titel TEXT, author TEXT, year INTEGER)
