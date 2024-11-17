89550012c1712f38fa7446416a9a9039dc34e1d4
Becky Rieger <brieger@ivytech.edu>
2024-11-17 15:14:42 -0600

    commit

--- API/application.py
+++ API/application.py
@@ -13,13 +13,35 @@ class Book(db.Model):
     publisher = db.Column(db.String(120), nullable=False) 
     
     def __repr__(self): 
-        return { 
-            'id': self.id, 
-            'book_name': self.book_name, 
-            'author': self.author, 
-            'publisher': self.publisher 
-            }
+        return f"{self.id} - {self.book_name} - {self.author} - {self.publisher}"
     
-@app.route('/Books')
+@app.route('/books')
 def get_books():
     books=Book.query.all()
+
+    output=[]
+    for book in books:
+        book_data={'book_name':book.book_name,'author':book.author, 'publisher':book.publisher}
+        output.append(book_data)
+    return{"books":output}
+
+@app.route('/books/ <id>')
+def get_book(id):
+    book=Book.query.get_or_404(id)
+    return {'book_name':book.book_name,'author':book.author, 'publisher':book.publisher}
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    book=Book(book_name=request.json['book_name'], author=request.json['author'], publisher=request.json['publisher'])
+    db.session.add(book)
+    db.session.commit()
+    return{'id':book.id}
+
+@app.route('/books/<id>', methods=['DELETE'])
+def delete_book(id):
+    book=Book.query.get(id)
+    if book is None:
+        return{"error": "not found"}
+    db.session.delete(book)
+    db.session.commit()
+    return {"message": "YAY!!!"}
