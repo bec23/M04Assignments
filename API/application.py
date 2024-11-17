9eaa0f5408029a1b2ce0160a5ceb608c56751ff6
Becky Rieger <brieger@ivytech.edu>
2024-11-17 13:53:28 -0600

    commit

--- API/application.py
+++ API/application.py
@@ -2,6 +2,20 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello!'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
+db=SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    book_name = db.Column(db.String(120), nullable=False) 
+    author = db.Column(db.String(120), nullable=False) 
+    publisher = db.Column(db.String(120), nullable=False) 
+    
+    def to_dict(self): 
+        return { 
+            'id': self.id, 
+            'book_name': self.book_name, 
+            'author': self.author, 
+            'publisher': self.publisher 
+            }
+    
